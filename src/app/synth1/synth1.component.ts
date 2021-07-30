7b7bda445f24ec3ec89271eb9e222357582bac91
Richard Lucas <richard@grupovoz.com.br>
2021-07-30 18:25:28 -0300

    WebAudio API Start

--- src/app/synth1/synth1.component.ts
+++ src/app/synth1/synth1.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-synth1',
   templateUrl: './synth1.component.html',
   styleUrls: ['./synth1.component.css']
 })
+
 export class Synth1Component implements OnInit {
 
   audioContext = new AudioContext();
 
-  constructor() { }
+  @ViewChild('audiof') audio: ElementRef<HTMLAudioElement> | undefined
 
-  ngOnInit(): void {
+  constructor() {
+
   }
 
+  ngOnInit(): void {
 
+  }
+
+  play_f(key: string): void {
+    console.log(this.audio)
+  }
 
 }
