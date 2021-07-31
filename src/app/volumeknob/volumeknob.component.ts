bc791ea694882839b3ddf12da966a4cade180ef5
Richard Lucas <richard@grupovoz.com.br>
2021-07-31 11:37:06 -0300

    Volume knob e oscillator 2

--- src/app/volumeknob/volumeknob.component.ts
+++ src/app/volumeknob/volumeknob.component.ts
@@ -1,5 +1,5 @@
-import { VolumeService } from './../services/volume.service';
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { VolumeService } from '../services/volume.service';
 
 @Component({
   selector: 'app-volumeknob',
@@ -11,11 +11,14 @@ export class VolumeknobComponent implements OnInit {
   @Input() volume_now? : any = 2
   @ViewChild('volume') volume: any
 
-  constructor() { private volumeService: VolumeService }
+  constructor(private volumeService: VolumeService) { }
 
   ngOnInit(): void {
+    this.volume_now = this.volumeService.get_volume()
   }
 
-
+  change_volume(new_vol: number): void {
+    this.volumeService.set_volume(new_vol)
+  }
 
 }
